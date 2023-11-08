a264b9f04d296444cd621382334d581feae334d4
SaakarX <saakarg615@gmail.com>
2023-11-08 18:11:15 +0530

    ⚒️ Fixed Validate middleware

--- middleware/validate.js
+++ middleware/validate.js
@@ -1,3 +1,8 @@
+const { Schema, ZodError } = require('zod');
+
+/**
+ * @param {Schema} schema Schema
+ */
 function validate(schema) {
   return async function (req, res, next) {
     try {
@@ -8,9 +13,15 @@ function validate(schema) {
       });
       return next();
     } catch (error) {
-      return res
-        .status(400)
-        .json({ message: error.issues[0].message, status: 'error' });
+      if (error instanceof ZodError)
+        return res
+          .status(400)
+          .json({ message: error.issues[0].message, status: 'error' });
+      else
+        return res.status(400).json({
+          message: `☠️ ${error?.message}` ?? 'Something went wrong!',
+          status: 'error',
+        });
     }
   };
 }
